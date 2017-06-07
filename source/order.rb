3fb4aae357f4f7d3e62ea1847ec79bf177542294
dogwood008 <dogwood008+github@gmail.com>
2017-06-07 14:49:13 +0900

    :+1: implement Order class

--- source/order.rb
+++ source/order.rb
@@ -1,10 +1,43 @@
 # frozen_string_literal: true
 
+require 'json'
+
 class Order
   attr_reader :price, :unit
 
   def initialize(price, unit)
     @price = price
     @unit = unit
+  end
+
+  def self.create_from_json(json_string)
+    j = JSON.parse(json_string, symbolize_names: true)
+    klass = get_buy_or_sell_class_by_name(j[:buy_or_sell])
+    klass.new(j[:price], j[:unit])
+  end
+
+  def to_h
+    {
+      price: @price,
+      unit: @unit,
+      buy_or_sell: buy_or_sell
+    }
+  end
+
+  def to_json
+    to_h.to_json
+  end
+
+  private
+
+  def get_buy_or_sell_class_by_name(buy_or_sell)
+    case buy_or_sell&.to_sym
+    when :buy
+      Order::Buy
+    when :sell
+      Order::Sell
+    else
+      raise ArgumentError
+    end
   end
 end
